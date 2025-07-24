7a0303ffd0be80c66f83617dc19c9a45b9d64c3a
Bert GvdE <vindigo.bgvde@gmail.com>
2025-07-24 22:19:16 +0200

    Header and navbar completed

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,55 @@
-import React from 'react'
+import { Button, Navbar, TextInput, NavbarCollapse, NavbarLink, NavbarToggle } from 'flowbite-react';
+import { Link, useLocation } from 'react-router-dom';
+import { AiOutlineSearch } from 'react-icons/ai';
+import { FaMoon } from 'react-icons/fa';
 
 export default function Header() {
+    const path = useLocation().pathname;
   return (
-    <div>Header</div>
+    <Navbar className='border-b-2'>
+        <Link to="/" className='self-center whitespace-nowrap text-sm sm:text-lg'>
+            <span className='px-2 py-1 bg-gradient-to-r from-blue-900 via-blue-600 to-blue-400 rounded-lg text-white'>Quinlan's</span>
+          <span className='text-blue-400 text-xl ml-2' >Blog</span>
+        </Link>
+        <form>
+            <TextInput 
+                type='text'
+                placeholder='Search...'
+                rightIcon={AiOutlineSearch}
+                className='hidden lg:inline'
+            />
+        </form>
+        <Button className='sm:lg lg:hidden py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-3xl border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700'>
+            <AiOutlineSearch />
+        </Button>
+        <div className='flex gap-2 md:order-2'>
+            <Button className='hidden sm:inline py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-3xl border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700'>
+                <FaMoon />
+            </Button>
+            <Link to="/sign-in" className='hidden sm:inline'>
+                <Button className='colorpy-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-2xl border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700'>
+                    Sign In
+                </Button>
+            </Link>
+            <NavbarToggle/>           
+        </div>
+        <NavbarCollapse>
+                <NavbarLink active={path === '/'} as={'div'}>
+                    <Link to='/'>
+                        Home
+                    </Link>
+                </NavbarLink>                
+                <NavbarLink active={path === '/projects'} as={'div'}>
+                    <Link to='/projects'>
+                        Projects
+                    </Link>
+                </NavbarLink>
+                <NavbarLink active={path === '/about'} as={'div'}>
+                    <Link to='/about'>
+                        About
+                    </Link>
+                </NavbarLink>
+            </NavbarCollapse>
+    </Navbar>
   )
 }
