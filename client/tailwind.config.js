ca559e78fc9aefa4a0bc62e8e2d4f656d6a63afb
Bert GvdE <vindigo.bgvde@gmail.com>
2025-07-24 20:23:11 +0200

    re-installing flowbite and adding header

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,12 +1,14 @@
+/* eslint-disable no-undef */
     /** @type {import('tailwindcss').Config} */
     export default {
       content: [
-        "./src/**/*.{html,js,ts,jsx,tsx}"
+        "./src/**/*.{html,js,ts,jsx,tsx}",
+        "./node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
       ],
       theme: {
         extend: {
         },
       },
-      plugins: [],
+      plugins: [require('flowbite/plugin')],
     }
 
