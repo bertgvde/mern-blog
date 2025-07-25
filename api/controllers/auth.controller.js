ee7166122627b52ed9131b8a63c6d5c0e7b261a4
Bert GvdE <vindigo.bgvde@gmail.com>
2025-07-25 16:38:33 +0200

    add middleware and function to handle errors

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,35 +1,30 @@
 import User from '../models/user.model.js';
 import bcryptjs from 'bcryptjs';
+import { errorHandler } from '../utils/error.js';
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
     const { username, password, email } = req.body;
     try {
         if (!username || !password || !email || username.trim() === '' || password.trim() === '' || email.trim() === '') {
-            return res.status(400).json({ message: 'Username, password and email are required' });
-        }
+            next(errorHandler(400, 'Username, password and email are required'));}
     } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: 'Server error' });       
+        next(errorHandler(500, 'Server error'));       
     }
 
     const hashedPassword = bcryptjs.hashSync(password, 10);
     if (!hashedPassword) {
-        return res.status(500).json({ message: 'Error hashing password' });
-    }
+        return next(errorHandler(500, 'Error hashing password'));}
 
     const newUser = new User({
         username,
         password: hashedPassword,
         email
     });
-    try {
-        const savedUser = await newUser.save();
-        res.status(201).json({ message: 'User created successfully', user: savedUser });
-    } catch (error) {
-        console.error(error);
-        if (error.code === 11000) {
-            return res.status(400).json({ message: 'Username already exists' });
-        }
-        res.status(500).json({ message: 'Server error' });
+try {
+    await newUser.save();
+    res.json({
+        success: true, message: 'User created successfully'})}
+    catch (error) {
+        next(errorHandler(409, 'User already exists'));
     }
-};
+}
